dc424f5a35c0bf8f5fbbed9492f1668cda0dfb0a
Ramiro Batista da Luz <ramiroluz@gmail.com>
2023-05-12 11:20:48 -0300

    Corrige links do Header e remove informações que eram do site do rodapé.

--- frontend/src/customizations/volto/components/theme/Header/Header.jsx
+++ frontend/src/customizations/volto/components/theme/Header/Header.jsx
@@ -67,11 +67,11 @@ import './Header.css';
             <div id="header-banner">
 
                 <div id="logo">
-                    <a href="https://www.curitiba.pr.leg.br" title="Portal da Câmara Municipal de Curitiba" accessKey="1">
+                    <a href="/" title="Portal da Câmara Municipal de Curitiba" accessKey="1">
                         <img src="https://www.curitiba.pr.leg.br/logo.png" alt="Portal da Câmara Municipal de Curitiba" title="Portal da Câmara Municipal de Curitiba" width="92" height="85" />
                     </a>
                     <h1>
-                        <a href="https://www.curitiba.pr.leg.br" title="Portal da Câmara Municipal de Curitiba" accessKey="1">
+                        <a href="/" title="Portal da Câmara Municipal de Curitiba" accessKey="1">
                             Câmara Municipal de <span>Curitiba</span>
                         </a>
                     </h1>
@@ -80,18 +80,18 @@ import './Header.css';
 
                 <ul id="siteactions">
 
-    <li id="siteaction-sitemap"><a href="https://www.curitiba.pr.leg.br/sitemap" accessKey="3" title="Mapa do Site">Mapa do Site</a></li>
-    <li id="siteaction-accessibility"><a href="https://www.curitiba.pr.leg.br/acessibilidade" accessKey="0" title="Acessibilidade">Acessibilidade</a></li>
-    <li id="siteaction-contato"><a href="https://www.curitiba.pr.leg.br/institucional/fale-com-a-camara" accessKey="" title="Contato">Contato</a></li>
+    <li id="siteaction-sitemap"><a href="/sitemap" accessKey="3" title="Mapa do Site">Mapa do Site</a></li>
+    <li id="siteaction-accessibility"><a href="/acessibilidade" accessKey="0" title="Acessibilidade">Acessibilidade</a></li>
+    <li id="siteaction-contato"><a href="/institucional/fale-com-a-camara" accessKey="" title="Contato">Contato</a></li>
     <li id="siteaction-vlibras"><a href="http://www.vlibras.gov.br/" accessKey="" title="VLibras">VLibras</a></li>
     <li id="siteaction-contraste"><a href="#" accessKey="" title="Contraste">Contraste</a></li>
-    <li id="siteaction-login"><a href="https://www.curitiba.pr.leg.br/login" accessKey="" title="Acessar">Acessar</a></li>
+    <li id="siteaction-login"><a href="/login" accessKey="" title="Acessar">Acessar</a></li>
 </ul>
 
 
                 <div id="portal-searchbox">
 
-    <form id="livesearch0" action="https://www.curitiba.pr.leg.br/@@search">
+    <form id="livesearch0" action="/@@search">
 
         <div className="LSBox">
         <label className="hiddenStructure" htmlFor="searchGadget">Busca</label>
@@ -107,7 +107,7 @@ import './Header.css';
     </form>
 
     <div id="portal-advanced-search" className="hiddenStructure">
-        <a href="https://www.curitiba.pr.leg.br/@@search" accessKey="5">Busca Avançada…</a>
+        <a href="/@@search" accessKey="5">Busca Avançada…</a>
     </div>
 </div>
             </div>
@@ -117,39 +117,6 @@ import './Header.css';
                         <i className="fas fa-times" aria-hidden="true"></i>
                         <span className="menu-title">Menu</span>
                     </a>
-
-                    <ul id="redes-sociais">
-                        <li>
-                            <a href="https://www.facebook.com/CamaraCuritiba/">
-                                <i className="fab fa-facebook-square" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://twitter.com/CamaraCuritiba">
-                                <i className="fab fa-twitter-square" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.instagram.com/camaradecuritiba/">
-                                <i className="fab fa-instagram" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.youtube.com/camaramunicipaldecuritiba">
-                                <i className="fab fa-youtube" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.flickr.com/photos/camaracuritiba/">
-                                <i className="fab fa-flickr" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://open.spotify.com/show/77uZ6a7lpDGWPnyMy2ABCo">
-                                <i className="fab fa-spotify" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                    </ul>
                     <ul id="destaques">
                         <li><a href="/institucional/fale-com-a-camara" title="Entre em contato">Entre em contato</a></li>
                         <li><a href="/transparencia/portal-da-transparencia-1" title="Portal da Transparência">Portal da Transparência</a></li>
