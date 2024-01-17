00fd71d145667affa4d7d84823f8450e74076dd8
Ramiro Batista da Luz <ramiroluz@gmail.com>
2024-01-17 12:14:40 -0300

    Reverte código do Footer.

--- frontend/src/customizations/volto/components/theme/Logo/Logo.jsx
+++ frontend/src/customizations/volto/components/theme/Logo/Logo.jsx
@@ -1,60 +1,144 @@
-/**
- * Logo component.
- * @module components/theme/Logo/Logo
- */
-import { useEffect } from 'react';
+// SemanticUI-free pre-@plone/components
+import React from 'react';
+
+import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
+import { useSelector, shallowEqual } from 'react-redux';
+import { UniversalLink, Logo } from '@plone/volto/components';
+import Container from '@kitconcept/volto-light-theme/components/Atoms/Container/Container';
+import { flattenToAppURL, addAppURL } from '@plone/volto/helpers';
+import config from '@plone/volto/registry';
 import { Image } from 'semantic-ui-react';
-import { ConditionalLink } from '@plone/volto/components';
-import LogoImage from './Logo.png';
-import { useSelector, useDispatch } from 'react-redux';
-import { useLocation } from 'react-router-dom';
-import { getNavroot } from '@plone/volto/actions';
-import {
-  flattenToAppURL,
-  hasApiExpander,
-  getBaseUrl,
-} from '@plone/volto/helpers';
+import LogoPreta from './logo-cmc-preta.png';
+
+const messages = defineMessages({
+  copyright: {
+    id: 'Copyright',
+    defaultMessage: 'Copyright',
+  },
+});
 
 /**
- * Logo component class.
- * @function Logo
+ * Component to display the footer.
+ * @function Footer
  * @param {Object} intl Intl object
- * @returns {string} Markup of the component.
+ * @returns {string} Markup of the component
  */
-const Logo = () => {
-  const pathname = useLocation().pathname;
-  const site = useSelector((state) => state.site.data);
-  const navroot = useSelector((state) => state.navroot.data);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (pathname && !hasApiExpander('navroot', getBaseUrl(pathname))) {
-      dispatch(getNavroot(getBaseUrl(pathname)));
-    }
-  }, [dispatch, pathname]);
-
-  const navRootPath = flattenToAppURL(navroot?.navroot?.['@id']) || '/';
-  const currentURLIsNavRoot = pathname !== navRootPath;
-
+const Footer = ({ intl }) => {
+  const { settings } = config;
+  const { lang, siteActions = [] } = useSelector(
+    (state) => ({
+      lang: state.intl.locale,
+      siteActions: state.actions?.actions?.site_actions,
+    }),
+    shallowEqual,
+  );
   return (
-    <ConditionalLink
-      href={navRootPath}
-      title={navroot?.navroot?.title}
-      // In case that the content returns 404, there is no information about the portal
-      // then render the link anyways to get out of the Unauthorized page
-      condition={!navroot || currentURLIsNavRoot}
-    >
-      <Image
-        src={
-          site['plone.site_logo']
-            ? flattenToAppURL(site['plone.site_logo'])
-            : LogoImage
-        }
-        alt={navroot?.navroot?.title}
-        title={navroot?.navroot?.title}
-      />
-    </ConditionalLink>
+    <footer id="footer">
+      <Container layout className="footer">
+        <div className="footer-message">
+          <div align="center">
+            <Image src={LogoPreta} />
+          </div>
+          <FormattedMessage
+            id="AQUI SE CONSTRÓI O FUTURO DA CIDADE   |   18ª LEGISLATURA (2021-2024)"
+            defaultMessage="AQUI SE CONSTRÓI O FUTURO DA CIDADE   |   18ª LEGISLATURA (2021-2024)"
+            values={{
+              plonecms: (
+                <FormattedMessage
+                  id="Plone{reg} Open Source CMS/WCM"
+                  defaultMessage="Plone{reg} Open Source CMS/WCM"
+                  values={{ reg: <sup>®</sup> }}
+                />
+              ),
+              copyright: (
+                <abbr title={intl.formatMessage(messages.copyright)}>©</abbr>
+              ),
+              current_year: new Date().getFullYear(),
+              plonefoundation: (
+                <a className="item" href="http://plone.org/foundation">
+                  <FormattedMessage
+                    id="Plone Foundation"
+                    defaultMessage="Plone Foundation"
+                  />
+                </a>
+              ),
+            }}
+          />{' '}
+          <br />
+          <FormattedMessage
+            id="Distributed under the {license}."
+            defaultMessage="Distributed under the {license}."
+            values={{
+              license: (
+                <a
+                  className="item"
+                  href="http://creativecommons.org/licenses/GPL/2.0/"
+                >
+                  <FormattedMessage
+                    id="GNU GPL license"
+                    defaultMessage="GNU GPL license"
+                  />
+                </a>
+              ),
+            }}
+          />
+        </div>
+        <ul>
+          {/* wrap in div for a11y reasons: listitem role cannot be on the <a> element directly */}
+          {siteActions?.length
+            ? siteActions.map((item) => (
+                <li className="item" key={item.id}>
+                  <UniversalLink
+                    className="item"
+                    href={
+                      settings.isMultilingual
+                        ? `/${lang}/${
+                            item.url
+                              ? flattenToAppURL(item.url)
+                              : addAppURL(item.id)
+                          }`
+                        : item.url
+                        ? flattenToAppURL(item.url)
+                        : addAppURL(item.id)
+                    }
+                  >
+                    {item?.title}
+                  </UniversalLink>
+                </li>
+              ))
+            : null}
+        </ul>
+        <div className="logo">
+          <Logo />
+        </div>
+        <a className="item powered-by" href="https://plone.org">
+          <FormattedMessage
+            id="Powered by Plone & Python"
+            defaultMessage="Powered by Plone & Python"
+          />
+        </a>
+        <br />
+        <div className="footer-branding">
+          Made with{' '}
+          <span role="img" aria-label="love" style={{ color: 'red' }}>
+            ❤️
+          </span>{' '}
+          by kitconcept
+        </div>
+      </Container>
+    </footer>
   );
 };
 
-export default Logo;
+/**
+ * Property types.
+ * @property {Object} propTypes Property types.
+ * @static
+ */
+Footer.propTypes = {
+  /**
+   * i18n object
+   */
+};
+
+export default injectIntl(Footer);
